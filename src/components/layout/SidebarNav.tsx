d33b41841b8c352e9d0459c5f03fa514e1941cb5
Bobby Kenner <bobby.kenner@grammarly.com>
2026-01-20 12:54:44 -0500

    Remove unused logo hover event handlers

--- src/components/layout/SidebarNav.tsx
+++ src/components/layout/SidebarNav.tsx
@@ -170,8 +170,6 @@ export function SidebarNav({ onNavigate }: SidebarNavProps) {
             <Link
               to="/"
               className="w-8 h-8 flex items-center justify-center flex-shrink-0 cursor-pointer"
-              onMouseEnter={() => setIsLogoHovered(true)}
-              onMouseLeave={() => setIsLogoHovered(false)}
             >
               <svg width="20" height="22" viewBox="0 0 20 22" fill="none" xmlns="http://www.w3.org/2000/svg" className="text-foreground">
                 <path d="M9.97852 0C16.7707 7.42797e-05 19.9561 3.48645 19.9561 11C19.956 18.5135 16.7707 21.9999 9.97852 22C3.18621 22 1.46999e-05 18.5136 0 11C0 3.48637 3.18620 0 9.97852 0ZM9.97852 4.35742C6.37198 4.35742 4.83887 6.19128 4.83887 11C4.83888 15.8087 6.37199 17.6416 9.97852 17.6416C13.6149 17.6415 15.1172 15.8086 15.1172 11C15.1172 6.19137 13.615 4.35749 9.97852 4.35742Z" fill="currentColor"/>
